fd1e3e17db7bb0f83bbe681f750d14d15182d37f
wintleh <hunter.wintle@gmail.com>
2020-06-11 17:03:18 -0400

    PC-44 Add interest permissions

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Interest
 from .serializers import InterestSerializer
@@ -6,3 +6,4 @@ from .serializers import InterestSerializer
 class InterestList(generics.ListAPIView):
     queryset = Interest.objects.all()
     serializer_class = InterestSerializer
+    permission_classes = [permissions.IsAuthenticated]

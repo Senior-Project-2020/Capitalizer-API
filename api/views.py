132e904a1cc452e6bd965ab25fdb42130a9bfbca
wintleh <hunter.wintle@gmail.com>
2020-06-11 20:42:32 -0400

    PC-41 Add stock view permissions

--- api/views.py
+++ api/views.py
@@ -8,22 +8,26 @@ class InterestList(generics.ListAPIView):
     serializer_class = InterestSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-# Create and list all stock prices
+# List all stock prices; Create reserved for users with special permissions set
 class StockPriceList(generics.ListCreateAPIView):
     queryset = StockPrice.objects.all()
     serializer_class = StockPriceSerializer
+    permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions]
 
-# Update and view individual stock prices
+# View individual stock prices; Update reserved for users with special permissions set
 class StockPriceDetail(generics.RetrieveUpdateAPIView):
     queryset = StockPrice.objects.all()
     serializer_class = StockPriceSerializer
+    permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions]
 
-# List all stocks
-class StockList(generics.ListAPIView):
+# List all stocks; Create reserved for users with special permissions set
+class StockList(generics.ListCreateAPIView):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions]
 
-# View individual stocks
-class StockDetail(generics.RetrieveAPIView):
+# View individual stocks; Update and destroy reserved for users with special permissions set
+class StockDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions]

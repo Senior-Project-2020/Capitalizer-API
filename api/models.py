4c7fcdeb7b6cc6256781b01116ccd2b590c08f95
wintleh <hunter.wintle@gmail.com>
2020-06-13 20:11:50 -0400

    PC-50 Remove interest choices
    Interests added/removed by admin

--- api/models.py
+++ api/models.py
@@ -1,14 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-INTERESTS = [
-    ('tech', 'Tech'), 
-    ('real_estate','Real Estate'), 
-    ('healthcare','Healthcare'),
-]
-
 class Interest(models.Model):
-    interest = models.CharField(choices=INTERESTS, max_length=100)
+    interest = models.CharField(max_length=100)
 
     def __str__(self):
         return self.interest

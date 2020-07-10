defa9cb3876938d6f04787e7706836d5269af775
wintleh <hunter.wintle@gmail.com>
2020-07-10 10:49:42 -0400

    Add pagination

--- app/Project_Capitalizer/settings.py
+++ app/Project_Capitalizer/settings.py
@@ -59,7 +59,9 @@ REST_AUTH_SERIALIZERS = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 500,
 }
 
 # django-cors-headers configuration
